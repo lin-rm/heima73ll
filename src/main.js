f949d77d03028731acf23f92ecec1c97b673ac36
lin-rm <lin_rongmei@126.com>
2019-07-09 20:43:21 +0800

    login界面完成

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-import router from '@/router'
+import router from './router/index.js'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Login from '@/views/login'
+import Login from '../views/login/index.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routers: [
+  routes: [
     { name: 'login', path: '/login', component: Login }
   ]
 })
